f3746de455da42108bd788b75aaa897d23265f81
theseankid <sean.ippolito@gmail.com>
2021-07-25 20:49:19 -0600

    add k-NN distance

--- distances/ellipsoid_distances.py
+++ distances/ellipsoid_distances.py
@@ -1,6 +1,8 @@
 import numpy as np
 from itertools import combinations
+from sklearn.neighbors import NearestNeighbors
 from helper_functions import euclidean_distance_matrix
+
 
 def SVD_ellipsoid_distances(data):
     n, d = data.shape
@@ -11,6 +13,8 @@ def SVD_ellipsoid_distances(data):
     sigma = .05
     s = np.ones(d)
     s[-1] = sigma
+
+    # creates oblate spheroid mappings at each point
     Qs = [V.T*s@V for V in Vs]
 
     dists = euclidean_distance_matrix(data, normalize=True)
@@ -28,3 +32,18 @@ def SVD_ellipsoid_distances(data):
         D[i,j] = 2*dists[i,j]/(np.sqrt(a1)+np.sqrt(a2))
 
     return D + D.T
+
+
+def neighbors_distance(data):
+    n, d = data.shape
+    assert n > 1, 'need to have more than one observation for distance calculation'
+
+    nbrs = NearestNeighbors(n_neighbors=n).fit(data)
+    # distance matrix for a ripser filtration
+    all_indices = nbrs.kneighbors(data)[-1]
+    
+    D = np.zeros(all_indices.shape)
+    for i, row in enumerate(all_indices):    
+        for idx, j in enumerate(row):
+            D[i, j] = idx
+    return D/(n-1)
--- distances/helper_functions.py
+++ distances/helper_functions.py
@@ -1,14 +1,26 @@
 import numpy as np
+from itertools import combinations
 
-def euclidean_distance_matrix(data, normalize = False):
+def euclidean_distance_matrix(data, normalize = False, symmetric = True):
+    """
+    data : a should be shape n by d array where n is the number of observations
+
+    normalize : True puts distances to [0,1] range
+
+    symmetric : False returns an upper triangular matrix, useful for sparse matrix representation and histograms
+    """
+
     n = data.shape[0]
     D = np.zeros([n,n])
     
     for i, j in combinations(np.arange(n),2):
         D[i,j] = np.linalg.norm(data[i] - data[j])
-    
+
+    if symmetric:
+        D = D + D.T
+
     if normalize:
-        return (D + D.T)/D.max()
-        
+        return D/D.max()
+
     else:
-        return (D + D.T)
+        return D
